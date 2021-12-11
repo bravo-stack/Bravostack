a31743c0b123794d186cb8368d90d8bf0015da0d
Bravo Stack <chukwuebukaonuh23@gmail.com>
2021-12-11 23:52:24 +0100

    navigation drawer interactivity with javascript and some css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,33 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.querySelector("#nav-menu");
+const navToggle = document.querySelector("#nav-toggle");
+const navClose = document.querySelector("#nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.style.bottom = "0"
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
+if(navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.style.bottom = "-100%"
+    })
+}
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav_link")
+
+navLink.forEach((item) => {
+    item.addEventListener("click", () => {
+        navMenu.style.bottom = "-100%"
+    })
+})
 
 
 /*==================== ACCORDION SKILLS ====================*/
