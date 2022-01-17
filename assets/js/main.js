1f080e1c6035e25bc08a6973d54decc2d092cabf
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-01-17 17:53:34 +0100

    styled the menu, to show current tab

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,9 +103,24 @@ currentYear.textContent = yearText.toString()
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+    const scrollY = window.pageYOffset
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 
 /*==================== SHOW SCROLL UP ====================*/ 
